99e73ca2cd8e58d4f546f534c7964615fc37544f
gcrocker1 <garnetcrocker@gmail.com>
2025-05-27 11:14:46 -0500

    Added player class, etc.

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
 
     while(1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0,0,0))
+        screen.fill('black')
+        
+        player.draw(screen)
 
         pygame.display.flip()
+        dt = clock.tick(60)/1000    # This ticks the game at 60 fps, and also saves the delta time (time between loop iterations/frames) in seconds into dt
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
